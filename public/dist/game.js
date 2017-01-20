bd684eabc86e3af791b4df39b5d33bbbaa42c67a
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-01-19 23:02:07 -0800

    Creates root of the scene graph

--- public/dist/game.js
+++ public/dist/game.js
@@ -2,3 +2,6 @@ var renderer = PIXI.autoDetectRenderer(288,512);
 var gameView = document.getElementById('gameView');
 
 gameView.appendChild(renderer.view);
+
+var stage = new PIXI.Container();
+var container = new PIXI.Container();
