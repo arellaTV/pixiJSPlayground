5885f21347134a14b340fae7a6558a3e3864a193
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-01-19 23:46:50 -0800

    Loads background image

--- public/dist/game.js
+++ public/dist/game.js
@@ -1,15 +1,21 @@
-var renderer = PIXI.autoDetectRenderer(432,768);
 var gameView = document.getElementById('gameView');
-
+var renderer = PIXI.autoDetectRenderer(432,768);
+var stage = new PIXI.Container();
 gameView.appendChild(renderer.view);
 
-var stage = new PIXI.Container();
-var container = new PIXI.Container();
-
 PIXI.loader
-  .add('images/spritesheet.png')
-  .load(setup);
+.add('images/spritesheet.png')
+.load(setup);
 
-var setup = function() {
-  
+function setup() {
+  var texture = PIXI.utils.TextureCache['images/spritesheet.png'];
+  var rectangle = new PIXI.Rectangle(0, 0, 144, 256);
+  texture.frame = rectangle;
+
+  var background = new PIXI.Sprite(texture);
+  background.scale.x = 3;
+  background.scale.y = 3;
+
+  stage.addChild(background);
+  renderer.render(stage);
 }
