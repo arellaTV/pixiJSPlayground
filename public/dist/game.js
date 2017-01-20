732192b7b3ec51e0e68fbfef62511b7d7a5b0a6a
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-01-20 04:42:54 -0800

    Bird flaps according to basic mouse control

--- public/dist/game.js
+++ public/dist/game.js
@@ -35,6 +35,24 @@ function setup() {
   var backgroundRectangle = new PIXI.Rectangle(0, 0, 144, 256);
   var backgroundTexture = new PIXI.Texture(texture, backgroundRectangle);
   var background = new PIXI.Sprite(backgroundTexture);
+  background.interactive = true;
+  background.on('mousedown', onButtonDown);
+  background.on('mouseup', onButtonUp);
+
+  var fall = true;
+
+  function onButtonDown() {
+    console.log('clicking!');
+    this.isdown = true;
+    fall = false;
+  }
+
+  function onButtonUp() {
+    console.log('released!');
+    this.isdown = false;
+    fall = true;
+  }
+
 
   var groundTexture = new PIXI.Texture.fromImage('images/ground.png');
   var ground = new PIXI.extras.TilingSprite(groundTexture, 144, 56);
@@ -42,27 +60,27 @@ function setup() {
 
   var pipes = new PIXI.Container();
 
-  // setTimeout(() => {
-  //   setInterval(() => {
-  //     var randomY = Math.round(Math.random() * 100) + 80;
-  //     if (pipes.children.length > 6) {
-  //       pipes.removeChildren(0, 2);
-  //     }
-  //     var topPipeRectangle = new PIXI.Rectangle(56, 323, 26, 160);
-  //     var topPipeTexture = new PIXI.Texture(texture, topPipeRectangle);
-  //     var topPipe = new PIXI.Sprite(topPipeTexture);
-  //     topPipe.position.y = -215;
-  //     topPipe.position.y += randomY;
-  //     topPipe.position.x = 144;
-  //
-  //     var bottomPipeRectangle = new PIXI.Rectangle(84, 323, 26, 160);
-  //     var bottomPipeTexture = new PIXI.Texture(texture, bottomPipeRectangle);
-  //     var bottomPipe = new PIXI.Sprite(bottomPipeTexture);
-  //     bottomPipe.position.y = randomY;
-  //     bottomPipe.position.x = 144;
-  //     pipes.addChild(bottomPipe, topPipe);
-  //   }, 1175);
-  // }, 4000);
+  setTimeout(() => {
+    setInterval(() => {
+      var randomY = Math.round(Math.random() * 100) + 80;
+      if (pipes.children.length > 6) {
+        pipes.removeChildren(0, 2);
+      }
+      var topPipeRectangle = new PIXI.Rectangle(56, 323, 26, 160);
+      var topPipeTexture = new PIXI.Texture(texture, topPipeRectangle);
+      var topPipe = new PIXI.Sprite(topPipeTexture);
+      topPipe.position.y = -215;
+      topPipe.position.y += randomY;
+      topPipe.position.x = 144;
+
+      var bottomPipeRectangle = new PIXI.Rectangle(84, 323, 26, 160);
+      var bottomPipeTexture = new PIXI.Texture(texture, bottomPipeRectangle);
+      var bottomPipe = new PIXI.Sprite(bottomPipeTexture);
+      bottomPipe.position.y = randomY;
+      bottomPipe.position.x = 144;
+      pipes.addChild(bottomPipe, topPipe);
+    }, 1175);
+  }, 4000);
 
   var ticker = new PIXI.ticker.Ticker();
   ticker.start();
@@ -73,6 +91,15 @@ function setup() {
     ground.tilePosition.x -= 1.2;
     for (var i = 0; i < pipes.children.length; i++) {
       pipes.children[i].position.x -= 1.2;
+    }
+    if (fall === true) {
+      if (player.position.y < 194) {
+        player.position.y += 1;
+      } else {
+        player.position.y = 194;
+      }
+    } else {
+      player.position.y -= 1;
     }
     ticker.update();
     renderer.render(stage);
