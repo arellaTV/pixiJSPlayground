c876d4230aa3abb6d33f3d70b445351cf194e898
Jeremiah Arella <jeremiah.arella@gmail.com>
2017-01-20 03:07:20 -0800

    Generates bottom pipes randomly

--- public/dist/game.js
+++ public/dist/game.js
@@ -2,6 +2,7 @@ PIXI.SCALE_MODES.DEFAULT = PIXI.SCALE_MODES.NEAREST;
 var gameView = document.getElementById('gameView');
 var renderer = new PIXI.WebGLRenderer(144,256, {resolution: 3});
 var stage = new PIXI.Container();
+
 gameView.appendChild(renderer.view);
 
 
@@ -16,11 +17,37 @@ function setup() {
   var backgroundTexture = new PIXI.Texture(texture, backgroundRectangle);
   var background = new PIXI.Sprite(backgroundTexture);
 
-  var floorRectangle = new PIXI.Rectangle(292, 0, 168, 56);
-  var floorTexture = new PIXI.Texture(texture, floorRectangle);
-  var floor = new PIXI.Sprite(floorTexture);
-  floor.position.y = 200;
+  var groundTexture = new PIXI.Texture.fromImage('images/ground.png');
+  var ground = new PIXI.extras.TilingSprite(groundTexture, 144, 56);
+  ground.y = 200;
 
-  stage.addChild(background, floor);
-  renderer.render(stage);
+  var pipes = new PIXI.Container();
+
+  setInterval(() => {
+    var randomY = Math.round(Math.random() * 150) + 25;
+    if (pipes.children.length > 3) {
+      pipes.removeChildAt(0);
+    }
+    console.log('pipes', pipes.children.length);
+    var bottomPipeRectangle = new PIXI.Rectangle(84, 323, 26, 160);
+    var bottomPipeTexture = new PIXI.Texture(texture, bottomPipeRectangle);
+    var bottomPipe = new PIXI.Sprite(bottomPipeTexture);
+    bottomPipe.position.y = randomY;
+    bottomPipe.position.x = 144;
+    pipes.addChild(bottomPipe);
+  }, 1200);
+
+  function animate() {
+    ground.tilePosition.x -= 1.2 ;
+    for (var i = 0; i < pipes.children.length; i++) {
+      pipes.children[i].position.x -= 1.2;
+    }
+    renderer.render(stage);
+    requestAnimationFrame(animate);
+  }
+
+  animate();
+  stage.scale.x = 1;
+  stage.scale.y = 1;
+  stage.addChild(background, pipes, ground);
 }
